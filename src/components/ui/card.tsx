8121b850d4f5167715c65e46c825c5459f9e12e9
suhridp <suhridbpaul@gmail.com>
2025-08-24 10:28:41 +0530

    product

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 
-import { cn } from "@/src/lib/utils";
-
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
-      className
-    )}
+    className="rounded-xl border bg-card text-card-foreground shadow"
     {...props}
   />
 ));
--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -3,18 +3,13 @@ import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
 import { ChevronDown } from "lucide-react";
 
-import { cn } from "@/src/lib/utils";
-
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
 >(({ className, children, ...props }, ref) => (
   <NavigationMenuPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative z-10 flex max-w-max flex-1 items-center justify-center",
-      className
-    )}
+    className="relative z-10 flex max-w-max flex-1 items-center justify-center"
     {...props}
   >
     {children}
